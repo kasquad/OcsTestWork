0ce98170046c727fa112c24e10e38c15ec801ada
Radmir <RaRDautov@stud.kpfu.ru>
2023-04-29 18:40:18 +0300

    Change version to .net 7

--- src/OcsTestWork.OrderApi/Program.cs
+++ src/OcsTestWork.OrderApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",new OpenApiInfo(){Title = "OcsTestWork.OrderApi"});
+    options.CustomSchemaIds(s => s.FullName);   
+});
 
 var app = builder.Build();
 
