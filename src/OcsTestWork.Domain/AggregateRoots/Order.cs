e2ef6792cde1eca9292395083d233cc5b40be4ca
Radmir <RaRDautov@stud.kpfu.ru>
2023-04-30 12:07:02 +0300

    Add entites to domain layer

---
+++ src/OcsTestWork.Domain/AggregateRoots/Order.cs
@@ -0,0 +1,14 @@
+﻿using OcsTestWork.Domain.Entities;
+using OcsTestWork.Domain.Primitives;
+
+namespace OcsTestWork.Domain.AggregateRoots;
+
+public class Order : AggregateRoot
+{
+    public Order(Guid id) : base(id)
+    {
+    }
+    
+    public int Status { get; set; }
+    public IEnumerable<OrderedProduct> OrderedProducts { get; set; }
+}
