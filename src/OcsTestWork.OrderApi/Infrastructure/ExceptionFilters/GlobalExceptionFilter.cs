33b3ef70ea4b59381ce9a84408947d26448c4a14
Radmir <RaRDautov@stud.kpfu.ru>
2023-04-30 10:07:37 +0300

    Add Order Api infrastructure extension
    Add OrderApi infrastructure  extension methods for swagger,http and global exception filter for more details when an internal server error occurs

---
+++ src/OcsTestWork.OrderApi/Infrastructure/ExceptionFilters/GlobalExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace OcsTestWork.OrderApi.Infrastructure.ExceptionFilters;
+
+public class GlobalExceptionFilter : ExceptionFilterAttribute
+{ 
+    public override void OnException(ExceptionContext context)
+    {
+        base.OnException(context);
+        var resultObject = new
+        {
+            ExceptionType = context.Exception.GetType().FullName,
+            Message = context.Exception.Message,
+            StatucCode = StatusCodes.Status500InternalServerError
+        };
+        var jsonResult = new JsonResult(resultObject);
+
+        context.Result = jsonResult;
+    }
+}
---
+++ src/OcsTestWork.OrderApi/Infrastructure/Extensions/WebApplicationBuilderExtension.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Microsoft.OpenApi.Models;
+using OcsTestWork.OrderApi.Infrastructure.ExceptionFilters;
+using OcsTestWork.OrderApi.Infrastructure.StartupFilters;
+
+namespace OcsTestWork.OrderApi.Infrastructure.Extensions;
+
+public static class WebApplicationBuilderExtension
+{
+    public static WebApplicationBuilder AddHttp(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers(options => options.Filters.Add<GlobalExceptionFilter>());
+
+        return builder;
+    }
+    
+    public static WebApplicationBuilder AddInfrastructure(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo() { Title = "OcsTestWork.OrderApi" });
+            options.CustomSchemaIds(s => s.FullName);
+            
+            // Add ability show comments above class in swagger
+            var xmlFileName = Assembly.GetExecutingAssembly().GetName().Name + ".xml";
+            var xmlFilePath = Path.Combine(AppContext.BaseDirectory, xmlFileName);
+            
+            options.IncludeXmlComments(xmlFilePath);
+        });
+
+
+        
+        
+        
+        builder.Services.AddSingleton<IStartupFilter, SwaggerStartupFilter>();
+        
+        return builder;
+    }
+}
